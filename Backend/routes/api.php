b9c9d80ab5b7ac17c184b5b6963445abcf8d1098
elnatacorrea <elnatacorrea@hotmail.com>
2025-01-14 22:28:37 -0400

    subindo o back

---
+++ Backend/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
+
+/*
+|---------------------------------------------------------------------------
+| API Routes
+|---------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('categorias')->group(function () {
+    Route::get('/', [CategoriaController::class, 'index']); 
+    Route::post('/', [CategoriaController::class, 'store']); 
+    Route::get('/{id}', [CategoriaController::class, 'show']);
+    Route::put('/{id}', [CategoriaController::class, 'update']); 
+    Route::delete('/{id}', [CategoriaController::class, 'destroy']); 
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutoController::class, 'index']); 
+    Route::post('/', [ProdutoController::class, 'store']); 
+    Route::get('/{id}', [ProdutoController::class, 'show']);
+    Route::put('/{id}', [ProdutoController::class, 'update']);
+    Route::delete('/{id}', [ProdutoController::class, 'destroy']);
+});
